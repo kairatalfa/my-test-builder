f7bcd54a7af8aa78d50f8021492290006e04571f
kairat <kairatkubanychbekov96@gmail.com>
2022-12-01 16:35:57 +0300

    Updates

---
+++ precache-manifest.1a62185fac18397baa000771b9226178.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dabc4a4bdb00d44994bd1f9d71e43cb5",
+    "url": "/my-test-builder/index.html"
+  },
+  {
+    "revision": "0d36efc58515ea4e17cd",
+    "url": "/my-test-builder/static/css/main.8783902b.chunk.css"
+  },
+  {
+    "revision": "8b8662dbb0edd3bee3a6",
+    "url": "/my-test-builder/static/js/2.dba5b813.chunk.js"
+  },
+  {
+    "revision": "849c37eac61260ac162d96acf228e002",
+    "url": "/my-test-builder/static/js/2.dba5b813.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d36efc58515ea4e17cd",
+    "url": "/my-test-builder/static/js/main.ce2e2dfa.chunk.js"
+  },
+  {
+    "revision": "eef2a4e205cf0de5fd18",
+    "url": "/my-test-builder/static/js/runtime-main.088eae03.js"
+  }
+]);
